dc355ea4d5d6e92fb0ef6397a36ab2a944929df0
webstylecenter <petervandam24@hotmail.com>
2016-01-17 22:56:53 +0100

    AvailableSeatsArray

--- functions/testcase_cinema.php
+++ functions/testcase_cinema.php
@@ -9,6 +9,7 @@ class Cinema
     private $seats;
     private $visitors;
     private $chosenSeats = array();
+    public $availableSeatsGroups = array();
 
     public $count = array(
         'x' => 0,
@@ -17,7 +18,8 @@ class Cinema
         'u' => 0,
         'f' => 0,
         'p' => 0,
-        'r'=> 0
+        'r'=> 0,
+        'fix'=>0
     );
 
 
@@ -26,7 +28,66 @@ class Cinema
 
         $this->maxSeats = $maxSeats;
         $this->createSeats();
+        $this->countAvailableSeats();
         $this->placeRandomPeople();
+
+    }
+
+    function countAvailableSeats()
+    {
+        $this->availableSeatsGroups[] = [
+            'min'=> 0,
+            'max'=>$this->maxSeats-1,
+            'size'=>$this->maxSeats-0
+        ];
+    }
+
+    function updateAvailableSeatsGroup($start, $amount, $isVisitor)
+    {
+
+        // echo '<p>Start</p>';
+        // print_r($this->availableSeatsGroups); echo '<br />';
+        foreach ($this->availableSeatsGroups as $i => $group){ $this->count['fix']++;
+
+            if ($group['min'] <= $start && $group['max'] >= $start) {
+
+                unset($this->availableSeatsGroups[$i]);
+
+                // Array ervoor
+                if (($start - $group['min']) !== 0 && ($start-$amount > 0)) {
+                    $this->availableSeatsGroups[] = [
+                        'min'=> $group['min'],
+                        'max'=> $start-$amount,
+                        'size'=> ($start - $group['min'])
+                    ];
+                }
+
+                // Plaats user
+                if ($isVisitor) {
+                    for ($x = 0; $x < $amount; $x++) {
+                        //echo 'Oud: ('.($start+$x).'-'.$amount.') '.$this->seats[($start+$x)]. ' -> new<br />';
+                        $this->seats[($start+$x)] = 'new';
+                        $this->chosenSeats[] = ($start+$x)+1;
+                    }
+                } else {
+                    $this->seats[$start] = 'taken';
+                }
+
+                // Array erna
+                if (($group['max'] - ($start)) > 0) {
+                    $this->availableSeatsGroups[] = [
+                        'min'=> $start+$amount,
+                        'max'=> $group['max'],
+                        'size'=> ($group['max'] - ($start))
+                    ];
+                }
+                //usort($this->availableSeatsGroups, 'sortBySize');
+                //print_r($this->availableSeatsGroups); echo '<br />';
+                return;
+            }
+        }
+
+
     }
 
     function giveSeatNumbers($visitors)
@@ -38,7 +99,6 @@ class Cinema
         $this->findBestPositions();
 
         return array(
-
             'Chosen seats' => $this->chosenSeats
         );
     }
@@ -46,8 +106,10 @@ class Cinema
     public function placeRandomPeople()
     {
         for ($i = 0; $i < $this->maxSeats; $i++) { $this->count['x']++;
-            if (rand(1, 4) == 1) {
-                $this->seats[$i] = 'taken';
+            if (rand(1, 2) == 1) {
+
+                $this->updateAvailableSeatsGroup($i, 1, false);
+
             }
         }
     }
@@ -67,47 +129,27 @@ class Cinema
 
     public function placeVisitorsTogether($amount)
     {
-        // TODO: $i++ oplossing vinden zodat het sneller werkt zonder fouten
-        for ($i = 0; $i < $this->maxSeats; $i=$i+$amount) { $this->count['y']++;
-            if ($this->isGroupEmpty($i, $amount)) {
-                $this->populateGroup($i, $amount);
+
+        foreach ($this->availableSeatsGroups as $i => $group){
+            if ($group['size'] >= $amount) { //echo 'Groote past ('.$amount.') ';
+                $this->updateAvailableSeatsGroup($group['min'], $amount, true);
                 return true;
             }
         }
         return false;
-    }
 
-    public function isGroupEmpty($startPosition, $amount)
-    {
-        for ($i = 0; $i < $amount; $i++) { $this->count['u']++;
-            $pos = $startPosition + $i;
-            if ($pos == $this->maxSeats) {
-                return false;
-            }
-            if ($this->seats[$pos] == 'taken' || $this->seats[$pos] == 'new') {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public function populateGroup($startPosition, $amount)
-    {
-        for ($i = 0; $i < $amount; $i++) { $this->count['f']++;
-            $pos = $startPosition + $i;
-            $this->seats[$pos] = 'new';
-            array_push($this->chosenSeats, $pos);
-        }
     }
 
     public function findBestPositions()
     {
+
         $visitorQueue = $this->visitors;
         while ($visitorQueue > 0) { $this->count['r']++;
             for ($i = $visitorQueue; $i > 0; $i--) { $this->count['p']++;
                 if ($this->placeVisitorsTogether($i)) {
+                    // echo '<p>vis'.$visitorQueue.'  - '.$i.'</p>';
                     $visitorQueue -= $i;
-                    break;
+                    $i = $visitorQueue;
                 }
             }
         }
@@ -118,16 +160,24 @@ class Cinema
         $seatUsage = array_count_values($this->seats);
         $output = '';
 
-
-       // $output .= '<p>Taken: '.$seatUsage['taken'].'<br />Available: '.$seatUsage['free'].'<br />Used: '.$seatUsage['new'].'</p>';
         for ($i = 0; $i < $this->maxSeats; $i++) {
             $output .= '<div class="seat ' . $this->seats[$i] . '">'
-             . ($i + 1) .
-            '</div>';
+                . ($i + 1) .
+                '</div>';
         }
         return $output;
     }
 
 }
+
+function sortBySize($x, $y)
+{
+    if ($x == $y) {
+        return 0;
+    } else {
+        return $x['min'] - $y['min'];
+    }
+}
+
 
 
