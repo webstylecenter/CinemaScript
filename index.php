3d8bd7fd4f9d4c2d09c50bac2f7149c2e4956dfc
webstylecenter <petervandam24@hotmail.com>
2016-02-03 21:39:30 +0100

    Changed some settings

--- index.php
+++ index.php
@@ -11,17 +11,19 @@
 <pre>
 <?php
 
+$start = microtime(true);
 
+$visitors = 16000;
+$maxAmount = 500000;
 $takenSeats = NULL;
 
-$visitors = 16;
-$cinema = new Cinema(30, $takenSeats);
+$cinema = new Cinema($maxAmount, $takenSeats);
 $cinema->giveSeatNumbers($visitors);
-
-print_r($cinema->chosenSeats);
 
 ?>
 </pre>
+
+<?php echo '<p>Took '.(microtime(true) - $start).' seconds to place '.$visitors.' into a Cinema of '.$maxAmount.'</p>'; ?>
 
 <div id="cinema">
     <?php $cinema->showSeats();  ?>
